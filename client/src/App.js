459a6f8f19911fc8780295425972bd6b905b45c9
Kat Kitay <katkitay@gmail.com>
2018-10-31 12:53:22 -0400

    updated more fun UI\!

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,13 @@ import StreamList from './StreamList';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Streaming Into the Void</h1>
+      <main className="App">
         <h4>You are flying through a sunless void searching for a way out. There are other lost souls here, too. You can see them, but they cannot see you. Some have perished. There are monsters.  You must press on through the dark.</h4>
         <StreamList />
-        <div className="credit">
+        <aside className="credit">
           made by <a href="https://twitter.com/kkitay">@kkitay</a>
-        </div>
-      </div>
+        </aside>
+      </main>
     );
   }
 }
--- client/src/StreamList.js
+++ client/src/StreamList.js
@@ -2,87 +2,119 @@ import React from 'react';
 import Stream from './Stream';
 import PouchDB from 'pouchdb';
 import PouchDBFind from 'pouchdb-find';
+import classNames from 'classnames';
 
-// Pouch DB setup
 PouchDB.plugin(PouchDBFind);
-const dbURL = process.env.REACT_APP_STAGE === 'dev'
-  ? 'http://localhost:5000/db/voids'
-  : 'https://api.void.kitay.co/voids';  
 
-const Voids = new PouchDB(dbURL);
+let env = process.env.REACT_APP_STAGE;
+const dbURL =
+  env === 'dev'
+    ? 'http://localhost:5000/voids'
+    : 'https://api.void.kitay.co/voids';
 
 class StreamList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fetching: true,
+      fetchSecs: 0,
       streams: [],
       seen: []
-    }
+    };
+    this.db = null;
+    this.interval = null;
   }
 
   randInterval(min, max) {
-      return Math.random()*(max-min+1)+min;
+    return Math.random() * (max - min + 1) + min;
   }
 
-  getStreams = (count = 2) => {
+  getStreams = (count = 8) => {
     try {
-      let queryAtTime = Date.now() - Math.floor(this.randInterval(0, 2) * 60 * 1000);
+      let queryAtTime =
+        Date.now() - Math.floor(this.randInterval(0, 2) * 60 * 1000);
       let query = {
         selector: {
-          _id: {
-            $nin: this.state.seen
-          },
           created_at: {
             $gte: queryAtTime
           }
+          // ,_id: {
+          //   $nin: this.state.seen
+          // }
         },
-        sort: ['created_at'],
         limit: count
       };
-      Voids.find(query).then((response) => {
-        let seen = this.state.seen.slice();
-        seen.push(...response.docs.reduce((ids, doc) => {
-          ids.push(doc._id);
-          return ids;
-        }, []));
-
-         this.setState({
+      this.db.find(query).then(response => {
+        // let seen = this.state.seen.slice();
+        // seen.push(
+        //   ...response.docs.reduce((ids, doc) => {
+        //     ids.push(doc._id);
+        //     return ids;
+        //   }, [])
+        // );
+        if (response.docs.length === 0) {
+          console.log('got length 0');
+          return this.getStreams();
+        }
+        this.setState({
           fetching: false,
-          streams: response.docs,
-          seen
+          fetchSecs: 0,
+          streams: response.docs
+          // seen
         });
+        clearInterval(this.interval);
       });
-      Voids.explain(query).then(console.log)
-    } catch(e) {
-      console.log(e);
+      this.db.explain(query).then(console.log);
+    } catch (e) {
+      // try again!!!
+      this.getStreams();
     }
-  }
+  };
+
+  tickTock = () => {
+    this.setState({ fetchSecs: this.state.fetchSecs + 1 });
+  };
 
   nextOnClick = () => {
+    // set fetching state
     this.setState({
       fetching: true
     });
+    // start an interval for our cute message
+    this.interval = setInterval(() => this.tickTock(), 1000);
+
+    // grab streams
     this.getStreams();
-  }
+  };
 
   async componentDidMount() {
-    this.getStreams();
+    try {
+      // Connect to PouchDB
+      this.db = await new PouchDB(dbURL);
+      console.log('connected to db');
+
+      // Grab streams
+      this.getStreams();
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
     return (
       <div className="StreamList">
         {this.state.streams.map((stream, index) => (
-          <Stream
-            key={stream.id + '-' + index}
-            data={stream}
-          />
+          <Stream key={stream.id + '-' + index} data={stream} />
         ))}
-        <button className="next" onClick={this.nextOnClick}>
+        <button
+          className={classNames('next', { loading: this.state.fetching })}
+          onClick={this.nextOnClick}
+        >
           {this.state.fetching
-          ? "searching..."
-          : "go deeper"}
+            ? this.state.fetchSecs < 3
+              ? 'communing with the void'
+              : 'the void is cranky...'
+            : 'go deeper'}
         </button>
       </div>
     );
