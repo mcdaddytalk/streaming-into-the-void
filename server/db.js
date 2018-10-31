24d15d2fa94880b6e6be0fbfbdf479d2b3a3503c
Kat Kitay <katkitay@gmail.com>
2018-10-31 11:03:02 -0400

    make server secure

--- server/db.js
+++ server/db.js
@@ -1,9 +1,8 @@
 console.log(`environment: ${process.env.NODE_ENV}`);
 
-const tmpPath = process.env.NODE_ENV === 'production'
-  ? '/tmp'
-  : __dirname + '/tmp';
-  
+const tmpPath =
+  process.env.NODE_ENV === 'production' ? '/tmp' : __dirname + '/tmp';
+
 const PouchDB = require('pouchdb-node').defaults({
   prefix: tmpPath + '/pouch/',
   auto_compaction: true
@@ -16,16 +15,19 @@ PouchDB.plugin(require('pouchdb-upsert'));
 
 const Voids = new PouchDB('voids');
 
+// Erase everything first (clear out old voids locally)
+// then create an index on `created_at`
 Voids.erase()
-.then(() => {
-  Voids.createIndex({
-    index: {
-      fields: ['created_at']
-    }
-  });
-})
-.catch(e => console.log(e));
+  .then(() => {
+    Voids.createIndex({
+      index: {
+        fields: ['created_at']
+      }
+    });
+  })
+  .catch(e => console.log(e));
 
+// Console log information
 Voids.info().then(info => console.log(info));
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -1,41 +1,40 @@
 const express = require('express');
 const cors = require('cors');
-const cron = require('node-cron');
 const fetchStreams = require('./fetchStreams');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const {PouchDB} = require('./db');
+const { PouchDB } = require('./db');
 const Config = require('./config.json');
 
-const tmpPath = process.env.NODE_ENV === 'production'
-  ? '/tmp'
-  : __dirname + '/tmp';
+const tmpPath =
+  process.env.NODE_ENV === 'production' ? '/tmp' : __dirname + '/tmp';
 
 // cron job for fetcher
 let batchNum = 0;
 let cursor = null;
-const schedule = cron.schedule(
-  '* * * * *',
-  async () => {
-    batchNum++;
-    let token = (batchNum % 2
+
+setInterval(async () => {
+  batchNum++;
+  let token =
+    batchNum % 2
       ? Config.token.access_token
-      : Config.alternate_token.access_token);
-    console.log('Scheduled batch', batchNum, token);
-    let result = await fetchStreams.fetchBatch(token, cursor);
-    cursor = result.cursor;
-  },
-  { scheduled: false }
-);
-schedule.start();
+      : Config.alternate_token.access_token;
+  console.log('Scheduled batch', batchNum, token);
+  let result = await fetchStreams.fetchBatch(token, cursor);
+  cursor = result.cursor;
+}, 60000);
 
 // add db route
-app.use('/', cors(), require('express-pouchdb')(PouchDB, {
-  inMemoryConfig: true,
-  logPath: tmpPath + '/log.txt'
-}));
+app.use(
+  '/',
+  cors(),
+  require('express-pouchdb')(PouchDB, {
+    logPath: tmpPath + '/log.txt',
+    configPath: tmpPath + '/pouchdb-config.json'
+  })
+);
 
 // server live
 app.listen(port, () => {
